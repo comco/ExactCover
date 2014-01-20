ac956c4b986cff12fde316b554714401a1df4d71
Krasimir Georgiev <void.unsigned@gmail.com>
2014-01-20 19:59:12 +0200

    Added quick and dirty graphviz dot export generation to dancing links network.

--- src/com/comco/exactcover/dancinglinks/DancingLinksNetwork.java
+++ src/com/comco/exactcover/dancinglinks/DancingLinksNetwork.java
@@ -16,7 +16,7 @@ public class DancingLinksNetwork implements Network {
 			this.root = root;
 		} else {
 			throw new IllegalArgumentException(
-					"The root of a dencing link network should be located at (0, 0).");
+					"The root of a dancing link network should be located at (0, 0).");
 		}
 	}
 
@@ -32,5 +32,38 @@ public class DancingLinksNetwork implements Network {
 	
 	public Node getRoot() {
 		return root;
+	}
+	
+	// convert the network to the graphviz format
+	public String toGraphVizDot() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("graph G {\n");
+		// Declare nodes
+		DancingLinksCol head = new DancingLinksCol(root);
+		for (Node headNode : head) {
+			DancingLinksRow row = new DancingLinksRow(headNode);
+			for (Node node : row) {
+				sb.append(String.format("_%d_%d [label=\"\"];\n",
+						node.getRow(), node.getCol()));
+			}
+		}
+
+		// Create links
+		for (Node headNode : head) {
+			if (headNode.getRow() > 0) {
+				for (Node node : new DancingLinksRow(headNode)) {
+					if (node.getCol() > 0 && node.getRight().getCol() > 0) {
+						Node next = node.getRight();
+						sb.append(String.format("_%d_%d -- _%d_%d;\n",
+								node.getRow(), node.getCol(), next.getRow(),
+								next.getCol()));
+					}
+				}
+			}
+		}
+		sb.append("}");
+		
+		return sb.toString();
 	}
 }
