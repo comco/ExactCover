1186cd4052685257ececaefe767e3a14c9134a66
Krasimir Georgiev <void.unsigned@gmail.com>
2014-01-22 19:25:21 +0200

    Fixed ProgramState to compile.

--- src/com/comco/exactcover/cli/ProgramState.java
+++ src/com/comco/exactcover/cli/ProgramState.java
@@ -1,7 +1,15 @@
 package com.comco.exactcover.cli;
 
+import com.comco.exactcover.dancinglinks.DancingLinksAlgorithmX;
 import com.comco.exactcover.readers.PuzzleReader;
 
 public class ProgramState {
-	private PuzzleReader
+	PuzzleReader reader;
+	DancingLinksAlgorithmX algorithm;
+	
+	public ProgramState(PuzzleReader reader, DancingLinksAlgorithmX algorithm) {
+		super();
+		this.reader = reader;
+		this.algorithm = algorithm;
+	}
 }
