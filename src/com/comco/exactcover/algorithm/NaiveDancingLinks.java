20f24bdce3d98e1488578865dcd06790f191e79a
Krasimir Georgiev <void.unsigned@gmail.com>
2014-02-05 01:06:49 +0200

    Removed a TODO.

--- src/com/comco/exactcover/algorithm/NaiveDancingLinks.java
+++ src/com/comco/exactcover/algorithm/NaiveDancingLinks.java
@@ -3,7 +3,6 @@ package com.comco.exactcover.algorithm;
 public class NaiveDancingLinks extends BasicDancingLinks {
 	public NaiveDancingLinks(ColumnNode head) {
 		super(head);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
